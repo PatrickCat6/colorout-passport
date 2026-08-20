50f8a7a5e729eba3e9bfcafff78296ad921a234d
PatrickCat6 <catpatrickcat@gmail.com>
2026-08-20 15:50:05 -0600

    seo: fix icon paths to existing files

--- app/layout.js
+++ app/layout.js
@@ -63,8 +63,8 @@ export const metadata = {
   },
   icons: {
     icon: [
-      { url: '/icons/icon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/icons/icon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
     apple: [{ url: '/icons/icon-192x192.png', sizes: '192x192' }],
   },
